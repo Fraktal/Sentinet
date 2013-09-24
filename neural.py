53d46df288142de0a224f2fe23259ff2d0e4b839
Todd Ervin <servin@unca.edu>
2013-09-24 15:54:24 -0400

    changed neurloab template file. Still working on groking basics of neural network

--- neural.py
+++ neural.py
@@ -1,12 +1,46 @@
-import numpy as np
+# -*- coding: utf-8 -*-
+""" 
+Example of use multi-layer perceptron
+=====================================
+
+Task: Approximation function: 1/2 * sin(x)
+
+"""
+
 import neurolab as nl
+import numpy as np
 
 # Create train samples
-input = np.random.uniform(-0.5, 0.5, (10, 2))
-target = (input[:, 0] + input[:, 1]).reshape(10, 1)
+x = np.linspace(-7, 7, 20)
+y = np.sin(x) * 0.5
 
-# Create network with 2 inputs, 5 neurons in input layer and 1 in output layer
-net = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5]], [5, 1])
+size = len(x)
 
-# Train process
-err = net.train(input, target, show=15)
+inp = x.reshape(size,1)
+tar = y.reshape(size,1)
+
+# Create network with 2 layers and random initialized
+net = nl.net.newff([[-7, 7]],[5, 1])
+
+# Train network
+error = net.train(inp, tar, epochs=500, show=100, goal=0.02)
+
+# Simulate network
+out = net.sim(inp)
+
+# Plot result
+import pylab as pl
+pl.subplot(211)
+pl.plot(error)
+pl.xlabel('Epoch number')
+pl.ylabel('error (default SSE)')
+
+x2 = np.linspace(-6.0,6.0,150)
+y2 = net.sim(x2.reshape(x2.size,1)).reshape(x2.size)
+
+y3 = out.reshape(size)
+
+pl.subplot(212)
+pl.plot(x2, y2, '-',x , y, '.', x, y3, 'p')
+pl.legend(['train target', 'net output'])
+pl.show() 
